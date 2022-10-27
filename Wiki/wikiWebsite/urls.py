6b1f8fb4e73414f5056c9d8cf0451e39cb8fa420
derekaj16 <dprodigy@byu.edu>
2022-10-27 15:52:28 -0600

    add view functions

--- Wiki/wikiWebsite/urls.py
+++ Wiki/wikiWebsite/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path, include
+from django.urls import path
 
-from .views import indexPageView
+from .views import aboutPageView, articlePageView, contactPageView, indexPageView, searchPageView, subscribePageView
 
 urlpatterns = [
     path('', indexPageView, name='index'),
+    path('about', aboutPageView, name='about'),
+    path('contact', contactPageView, name='contact'),
+    path('subscribe', subscribePageView, name='subscribe'),
+    path('search', searchPageView, name='search'),
+    path('article', articlePageView, name='article'),
 ]
--- Wiki/wikiWebsite/views.py
+++ Wiki/wikiWebsite/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+
 # Create your views here.
 
 def indexPageView(request) :
     return HttpResponse('WE IS SMART')
+
+def articlePageView(request) : # add params
+    return HttpResponse('article page view')
+
+def aboutPageView(request) :
+    return HttpResponse('about page view')
+
+def contactPageView(request) :
+    return HttpResponse('contact page view')
+
+def searchPageView(request) :
+    return HttpResponse('search page view')
+
+def subscribePageView(request) :
+    return HttpResponse('subscribe page view')
