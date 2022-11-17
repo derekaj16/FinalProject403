72f2b5198a892bb1f773bc0e1dea319ca46abb2e
derekaj16 <dprodigy@byu.edu>
2022-11-17 14:18:20 -0700

    models

--- Wiki/wikiWebsite/admin.py
+++ Wiki/wikiWebsite/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Person, Subscriber, Article, Comment, Paragraph
 
 # Register your models here.
+admin.site.register(Person)
+admin.site.register(Subscriber)
+admin.site.register(Article)
+admin.site.register(Comment)
+admin.site.register(Paragraph)
--- Wiki/wikiWebsite/models.py
+++ Wiki/wikiWebsite/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Person(models.Model) :
+    first_name = models.CharField(max_length=50, blank=False)
+    last_name = models.CharField(max_length=50, blank=False)
+    email = models.EmailField(blank=False)
+
+    def __str__(self) :
+        return self.full_name
+
+    @property
+    def full_name(self) :
+        return '%s %s' % (self.first_name, self.last_name)
+
+    def save(self) :
+        self.first_name = self.first_name.title()
+        self.last_name = self.last_name.title()
+        super(Person, self).save()
+
+class Article(models.Model) :
+    header = models.CharField(max_length=30)
+    subheader = models.CharField(max_length=50)
+    authors = models.ManyToManyField(Person)
+
+    def __str__(self) :
+        return self.header
+
+class Subscriber(models.Model) :
+    personID = models.ForeignKey(Person, on_delete=models.CASCADE)
+    dateSubscribed = models.DateField(default=datetime.today)
+
+    def __str__(self) :
+        return str(self.personID)
+
+class Comment(models.Model) :
+    commentText = models.TextField(blank=False)
+    personID = models.ForeignKey(Person, on_delete=models.CASCADE)
+    articleID = models.ForeignKey(Article, on_delete=models.CASCADE)
+
+class Paragraph(models.Model) :
+    paragraphText = models.TextField(blank=False)
+    articleID = models.ForeignKey(Article, on_delete=models.CASCADE)
+
